d3100750e6ab5a4b8b28d416ec5f1f1c64e37603
RuelasLopezGuillermoAdiel <233060610328@cetis61.edu.mx>
2026-03-23 10:54:10 -0500

    nuevo

---
+++ src/modelos/Tareas.py
@@ -0,0 +1,23 @@
+from .database import Database 
+
+class TareaModel: 
+    def __init__(self):
+        self.db = DataBase()
+        
+    def listar_por_usuario(self, id_usuario):
+        conn = self.db.get_connection()
+        cursor = conn.cursor(dictionary=True)
+        query = "SELECT * FROM tareas WHERE id_usurio = %5 ORDER BY fecha_limite ASC"
+        cursor.execute(query, (id_usuario,))
+        resultado = cursor.fetchall()
+        conn.close()
+        return resultado
+    
+def crear(self, id_usuario, titulo, descripcion, prioridad, clasificacion):
+    conn = self.db.get_connection()
+    cursor = conn.cursor()
+    query = """INSERT INTO tareas (id_usuario, titulo, descripcion, prioridad, clasificacion)
+    VALUES (%5, %5, %5, %5, %5)"""
+    cursor.execute(query, (id_usuario, titulo, descripcion, prioridad, clasificacion))
+    conn.commit()
+    conn.close()
---
+++ src/modelos/Usuario.py
@@ -0,0 +1,22 @@
+import bcrypt
+from .database import Database 
+
+class UsuarioModel:
+    def __init__(self):
+        self.db = Fatabase()
+        
+    def registrar(self, usuario_database):
+        #encriptar contraseña
+        salt = bcrypt.gensalt()
+        hashed_pw = bcrypt.hashpw(usuario_data.password.encode('utf-8'), salt)
+        
+        conn = self.db.get_connectioon()
+        cursor = conn.cursors()
+        try:
+            cursor.execute(
+                "INSET INTO usuario (nombre, email, password) VALUES (%5, %5, %5)",
+                (usuario_data.nombre, usuario_data.email, hashed_pw.decode('utf-8'))
+            )
+            conn.commit()
+            return True
+    except Exception as e:
