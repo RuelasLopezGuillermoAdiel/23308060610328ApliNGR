2ba92c45233152f85ff8505e74ebe2f39dce8409
RuelasLopezGuillermoAdiel <23308060610328@cetis61.edu.mx>
2026-04-20 11:43:09 -0500

    nose

--- src/views/LoginViews.py
+++ src/views/LoginViews.py
@@ -7,3 +7,14 @@ def LoginView(page: ft.Page, auth_controller):
         border_radius=10,
         keyboard_type=ft.KeyboardType
     )
+    
+    pass_input = ft.TextField(
+        label="Contraseña",
+        password=True,
+        can_reveal_password=True,
+        width=350,
+        border_radius=10
+    )
+    
+    def login_click(e):
+        if not gmail.input.value
