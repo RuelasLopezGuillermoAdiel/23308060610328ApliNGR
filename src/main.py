0e342cda1ec9248424e748e78346b5883c0b4136
RuelasLopezGuillermoAdiel <23308060610328@cetis61.edu.mx>
2026-05-05 22:48:03 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,49 +1,50 @@
-import flet as ft
-from controllers.UserController import AuthController
-from controllers.TareasController import TareaController
-from views.LoginViews import LoginView
-from views.Dashboard import DashboardView
-
+iimport flet as ft
+from controllers.UsuariosController import AuthController
+from controllers.TareaController import TareaController
+from view.LoginView import LoginView
+from view.RegisterView import RegisterView  
+from view.DashboardView import DashboardView   
+from view.UsuarioView import UserView
 
 def start(page: ft.Page):
-    
-    page.title = "SIstema SIGE"
-    page.window:width = 450
-    page.window_height = 700
-    
+    page.title = "Sistema SIGE"
     auth_ctrl = AuthController()
-    task_ctrl = TareaController()
+    tarea_ctrl = TareaController()
 
     def route_change(e):
-        
         page.views.clear()
-        
         if page.route == "/":
-            page.views.append(LoginView(page,auth_ctrl))
-        
+            page.views.append(LoginView(page, auth_ctrl))
+        elif page.route == "/register": 
+            page.views.append(RegisterView(page, auth_ctrl))
         elif page.route == "/dashboard":
-            page.views.append(DashboardView(page, task_ctrl))
-                
+            page.views.append(DashboardView(page, tarea_ctrl))
+        elif page.route == "/perfil":
+            page.views.append(UserView(page, auth_ctrl))
+        
+        if not page.views:
+            page.views.append(
+                ft.View("/", [ft.Text("Error: Ruta no encontrada o vista vacía")])
+            )
+
         page.update()
+        
     def view_pop(e):
         if len(page.views) > 1:
             page.views.pop()
             top_view = page.views[-1]
             page.go(top_view.route)
+            
     page.on_route_change = route_change
     page.on_view_pop = view_pop
-    
-    
-    print("Iniciando navegacion...")
+
     if page.route == "/":
         route_change(None)
     else:
         page.go("/")
     
 def main():
-    ft.app(target=start)
+    ft.app(start)
 
-
-# punto de entrada correcto
 if __name__ == "__main__":
-    ft.app(target=start)
+    main()
